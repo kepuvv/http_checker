7c80e03045987f2ef0d1ef41b09fa0c3eff18317
Kepuvv <renard.ru@gmail.com>
2018-03-02 10:17:49 +0300

    http url check + ip generator v.0.1

---
+++ http_checker.rb
@@ -0,0 +1,40 @@
+require 'net/http'
+
+def working_url?(url_str)
+  url = URI.parse(url_str)
+  Net::HTTP.start(url.host, url.port) do |http|
+  http.head(url.request_uri).code == '200'
+  end
+rescue
+  false
+end
+
+income = File.open "ping.txt", "a"
+
+"176".upto("178") do |a|
+	
+	"113".upto("254") do |b|
+						
+		"001".upto("254") do |c|
+			
+			"001".upto("254") do |x|
+									
+			address = "#{a}#{b}#{c}#{x}"
+									
+			url = "http://" + address
+									
+			puts "Checking #{url}"
+												
+				if working_url? url
+				#puts "Work!"
+				income.write "#{url}\n"
+				end
+									
+			end
+		end
+	end
+end
+
+income.close
+
+puts "Finish"
